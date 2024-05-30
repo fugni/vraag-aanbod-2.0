6c38447d5436333844f70725e95049eab8a7b3fb
fungi <sylvienuijtemans@gmail.com>
2024-05-30 13:09:48 +0200

    .dotenvified

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mysql = require('mysql');
 
+require('dotenv').config(); 
+
 const app = express();
-const port = 5002;
+const port = process.env.PORT; 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'vraag_aanbod'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
